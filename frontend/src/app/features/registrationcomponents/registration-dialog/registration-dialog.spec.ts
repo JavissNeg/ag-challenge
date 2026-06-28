3df05358602d9ca6e26510bdf4bc58fe557dd604
Javiss <javiss@2806-107e-0010-a737-0000-0000-0000-0002.ipv6.infinitum.net.mx>
2026-06-28 16:27:47 -0600

    feat(frontend): implement registration dialog component

---
+++ frontend/src/app/features/registrationcomponents/registration-dialog/registration-dialog.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrationDialog } from './registration-dialog';
+
+describe('RegistrationDialog', () => {
+  let component: RegistrationDialog;
+  let fixture: ComponentFixture<RegistrationDialog>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegistrationDialog],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RegistrationDialog);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/features/registrationcomponents/registration-dialog/registration-dialog.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+  
+import { RegistrationForm } from '../../registration/components/registration-form/registration-form';
+
+
+@Component({
+  selector: 'app-registration-dialog',
+  standalone: true,
+  templateUrl: './registration-dialog.html',
+  styleUrl: './registration-dialog.scss',
+  imports: [MatDialogModule, MatButtonModule, RegistrationForm],
+})
+export class RegistrationDialog {
+  private readonly dialogRef = inject(MatDialogRef<RegistrationDialog>);
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    // TODO: Guardar alumno
+  }
+}
